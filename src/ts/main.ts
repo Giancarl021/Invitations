3ca6b87b74fd62137be149281152a9bc827b56ce
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-05-24 01:19:23 -0300

    Modified TS to JSON

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,13 @@
 import '../scss/main.scss';
 
 import Invitation from './models/invitation';
-import constants from './util/constants';
+import constants from '../util/constants.json';
 
 import Card from './services/card';
 
 async function main() {
     const card = Card();
-    
+
     const response = await fetch(constants.invitationUrl);
     const invitation: Invitation = await response.json();
 
--- src/ts/util/constants.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    invitationUrl: '/data/invitations.json',
-}
